2a036ffe0acdc2594d42e2112be8315da01e6093
draco_runner <12946551+DracoRunner@users.noreply.github.com>
2026-04-21 17:08:10 +0800

    chore: bump Formula to v0.2.0

--- Formula/avm.rb
+++ Formula/avm.rb
@@ -1,26 +1,26 @@
 class Avm < Formula
   desc "Lightweight local/global command alias manager (like asdf/nvm for aliases)"
   homepage "https://github.com/DracoRunner/avm"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DracoRunner/avm/releases/download/v0.1.0/avm_darwin_arm64.tar.gz"
-      sha256 "9cb4b56a2e5534e364fd2e870d8b8ac70a175378005936b5bbb65d07171e2af6"
+      url "https://github.com/DracoRunner/avm/releases/download/v0.2.0/avm_darwin_arm64.tar.gz"
+      sha256 "bf13a6e7759551d1cdc0396d8029bbacdee871cfc5a5bc671d6c7f1d488b444e"
     else
-      url "https://github.com/DracoRunner/avm/releases/download/v0.1.0/avm_darwin_amd64.tar.gz"
-      sha256 "d7c95fc37f920492eb5aa27cd7543b479d102bc156c09d9fdde59124ec2aa4c7"
+      url "https://github.com/DracoRunner/avm/releases/download/v0.2.0/avm_darwin_amd64.tar.gz"
+      sha256 "c7e09b94f22ddc70afbe41588994ca1146d0b4aaa42e5cc1d79a86d4041a1ff2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/DracoRunner/avm/releases/download/v0.1.0/avm_linux_arm64.tar.gz"
-      sha256 "febfe3d29db3c127b0af15fba7a9e121a04a53391a41a9017fdc2fa2e1c48caf"
+      url "https://github.com/DracoRunner/avm/releases/download/v0.2.0/avm_linux_arm64.tar.gz"
+      sha256 "6cb614618db6fb5f61454d27c766db9b41f01a1ee19e9e6a7ab9d65c7d9240be"
     else
-      url "https://github.com/DracoRunner/avm/releases/download/v0.1.0/avm_linux_amd64.tar.gz"
-      sha256 "3b552a3750a5eb24ffa6f839875831652d57e9ad6675ff118671e99010c9b08f"
+      url "https://github.com/DracoRunner/avm/releases/download/v0.2.0/avm_linux_amd64.tar.gz"
+      sha256 "41755aeb8aa4f114b5266eb7a6f4b7c2cdd66349a2661408934d79c486327f0b"
     end
   end
 
